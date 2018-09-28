d7df8531696e5e0d37411c046493fe24aa64109d
Anton Belyaev <xantbel@gmail.com>
2018-09-28 18:55:10 +0300

    Add basic ff_deposit

---
+++ apps/ff_transfer/src/ff_deposit.erl
@@ -0,0 +1,121 @@
+%%%
+%%% Withdrawal
+%%%
+
+-module(ff_deposit).
+
+-type id()          :: ff_transfer_machine:id().
+-type source()      :: ff_source:id(_).
+-type wallet()      :: ff_wallet:id(_).
+
+-type deposit() :: ff_transfer:transfer(transfer_params()).
+-type transfer_params() :: #{
+    source      := source(),
+    destination := wallet()
+}.
+
+-type machine() :: ff_transfer_machine:st(transfer_params()).
+-type events()  :: ff_transfer_machine:events().
+
+-export_type([deposit/0]).
+-export_type([machine/0]).
+-export_type([transfer_params/0]).
+-export_type([events/0]).
+
+%% Accessors
+
+-export([source/1]).
+-export([destination/1]).
+-export([id/1]).
+-export([source_acc/1]).
+-export([destination_acc/1]).
+-export([body/1]).
+-export([status/1]).
+
+%% API
+-export([create/3]).
+-export([get/1]).
+-export([get_machine/1]).
+-export([events/2]).
+
+%% Pipeline
+
+-import(ff_pipeline, [do/1, unwrap/1, unwrap/2, valid/2]).
+
+%% Accessors
+
+-spec source(deposit())          -> source().
+-spec destination(deposit())     -> wallet().
+-spec id(deposit())              -> ff_transfer:id().
+-spec source_acc(deposit())      -> ff_account:account().
+-spec destination_acc(deposit()) -> ff_account:account().
+-spec body(deposit())            -> ff_transfer:body().
+-spec status(deposit())          -> ff_transfer:status().
+
+source(T)          -> maps:get(source, ff_transfer:params(T)).
+destination(T)     -> maps:get(destination, ff_transfer:params(T)).
+id(T)              -> ff_transfer:id(T).
+source_acc(T)      -> ff_transfer:source(T).
+destination_acc(T) -> ff_transfer:destination(T).
+body(T)            -> ff_transfer:body(T).
+status(T)          -> ff_transfer:status(T).
+
+
+%%
+
+-define(NS, 'ff/deposit_v1').
+
+-type ctx()    :: ff_ctx:ctx().
+-type params() :: #{
+    source      := ff_source:id(),
+    destination := ff_wallet_machine:id(),
+    body        := ff_transaction:body()
+}.
+
+-spec create(id(), params(), ctx()) ->
+    ok |
+    {error,
+        {source, notfound | unauthorized} |
+        {destination, notfound} |
+        {provider, notfound} |
+        exists |
+        _TransferError
+    }.
+
+create(ID, #{source := SourceID, destination := DestinationID, body := Body}, Ctx) ->
+    do(fun() ->
+        Source = ff_source:get(unwrap(source, ff_source:get_machine(SourceID))),
+        Destination = ff_wallet_machine:wallet(unwrap(destination, ff_wallet_machine:get(DestinationID))),
+        ok = unwrap(source, valid(authorized, ff_source:status(Source))),
+        Params = #{
+            type        => deposit,
+            source      => ff_source:account(Source),
+            destination => ff_wallet:account(Destination),
+            body        => Body,
+            params      => #{
+                source      => SourceID,
+                destination => DestinationID
+            }
+        },
+        unwrap(ff_transfer_machine:create(?NS, ID, Params, Ctx))
+    end).
+
+-spec get(machine()) ->
+    deposit().
+
+get(St) ->
+    ff_transfer_machine:transfer(St).
+
+-spec get_machine(id()) ->
+    {ok, machine()}       |
+    {error, notfound}.
+
+get_machine(ID) ->
+    ff_transfer_machine:get(?NS, ID).
+
+-spec events(id(), machinery:range()) ->
+    {ok, events()} |
+    {error, notfound}.
+
+events(ID, Range) ->
+    ff_transfer_machine:events(?NS, ID, Range).
---
+++ apps/ff_transfer/src/ff_source.erl
@@ -0,0 +1,98 @@
+%%%
+%%% Source
+%%%
+%%% TODOs
+%%%
+%%%  - We must consider withdrawal provider terms ensure that the provided
+%%%    Resource is ok to withdraw to.
+%%%
+
+-module(ff_source).
+
+-type ctx()      :: ff_ctx:ctx().
+-type id()       :: ff_instrument:id().
+-type name()     :: ff_instrument:name().
+-type account()  :: ff_account:account().
+-type identity() :: ff_identity:id().
+-type currency() :: ff_currency:id().
+-type status()   :: ff_identity:status().
+-type resource() :: #{
+    type    := internal,
+    details => binary()
+}.
+
+-type source()      :: ff_instrument:instrument(resource()).
+-type params()      :: ff_instrument_machine:params(resource()).
+-type machine()     :: ff_instrument_machine:st(resource()).
+
+-export_type([id/0]).
+-export_type([source/0]).
+-export_type([status/0]).
+-export_type([resource/0]).
+
+%% Accessors
+
+-export([account/1]).
+-export([id/1]).
+-export([name/1]).
+-export([identity/1]).
+-export([currency/1]).
+-export([resource/1]).
+-export([status/1]).
+
+%% API
+
+-export([create/3]).
+-export([get_machine/1]).
+-export([get/1]).
+-export([is_accessible/1]).
+
+%% Accessors
+
+-spec id(source())       -> id().
+-spec name(source())     -> name().
+-spec account(source())  -> account().
+-spec identity(source()) -> identity().
+-spec currency(source()) -> currency().
+-spec resource(source()) -> resource().
+-spec status(source())   -> status().
+
+id(Source)       -> ff_instrument:id(Source).
+name(Source)     -> ff_instrument:name(Source).
+identity(Source) -> ff_instrument:identity(Source).
+currency(Source) -> ff_instrument:currency(Source).
+resource(Source) -> ff_instrument:resource(Source).
+status(Source)   -> ff_instrument:status(Source).
+account(Source)  -> ff_instrument:account(Source).
+
+%% API
+
+-define(NS, 'ff/source_v1').
+
+-spec create(id(), params(), ctx()) ->
+    ok |
+    {error,
+        _InstrumentCreateError |
+        exists
+    }.
+
+create(ID, Params, Ctx) ->
+    ff_instrument_machine:create(?NS, ID, Params, Ctx).
+
+-spec get_machine(id()) ->
+    {ok, machine()}       |
+    {error, notfound} .
+get_machine(ID) ->
+    ff_instrument_machine:get(?NS, ID).
+
+-spec get(machine()) ->
+    source().
+get(Machine) ->
+    ff_instrument_machine:instrument(Machine).
+
+-spec is_accessible(source()) ->
+    {ok, accessible} |
+    {error, ff_party:inaccessibility()}.
+
+is_accessible(Source) ->
+    ff_instrument:is_accessible(Source).

2e83eb06a576030b0225a270efe9de471e172041
Anton Belyaev <arentrue@users.noreply.github.com>
2018-10-05 17:40:10 +0300

    Fix ff_server childspec (#17)

--- apps/ff_transfer/test/ff_transfer_SUITE.erl
+++ apps/ff_transfer/test/ff_transfer_SUITE.erl
@@ -373,7 +373,7 @@ admin_call(Fun, Args) ->
     Request = {Service, Fun, Args},
     Client  = ff_woody_client:new(#{
         url           => <<"http://localhost:8022/v1/admin">>,
-        event_handler => woody_event_handler_default
+        event_handler => scoper_woody_event_handler
     }),
     ff_woody_client:call(Client, Request).
 

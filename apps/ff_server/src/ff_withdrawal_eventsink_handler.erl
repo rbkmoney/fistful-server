853659ca8f72ac240fb99e96174aee1ec1e787b7
Артем <WWW_cool@inbox.ru>
2018-11-06 14:47:44 +0300

    HG-384: Update fistful proto (#32)

--- apps/ff_server/src/ff_withdrawal_eventsink_handler.erl
+++ apps/ff_server/src/ff_withdrawal_eventsink_handler.erl
@@ -81,11 +81,11 @@ publish_event(#{
     }
 }) ->
     #'wthd_SinkEvent'{
-        'sequence'      = marshal(event_id, ID),
+        'id'            = marshal(event_id, ID),
         'created_at'    = marshal(timestamp, Dt),
         'source'        = marshal(id, SourceID),
         'payload'       = #'wthd_Event'{
-            'id'         = marshal(event_id, EventID),
+            'sequence'   = marshal(event_id, EventID),
             'occured_at' = marshal(timestamp, EventDt),
             'changes'    = [marshal(event, ff_transfer:maybe_migrate(Payload))]
         }

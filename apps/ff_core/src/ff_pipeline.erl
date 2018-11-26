cf13aed4cc7ef43a5035c5eeadb05555bf6feb75
Andrey Fadeev <me@ciiol.net>
2018-11-26 17:28:24 +0300

    Rewrite parse transform with erl_syntax and merl

--- apps/ff_core/src/ff_pipeline.erl
+++ apps/ff_core/src/ff_pipeline.erl
@@ -7,6 +7,8 @@
 
 -module(ff_pipeline).
 
+-include_lib("syntax_tools/include/merl.hrl").
+
 -export([do/1]).
 -export([do/2]).
 -export([unwrap/1]).
@@ -154,90 +156,40 @@ with(Model, St, F) ->
 -spec parse_transform(Forms, [compile:option()]) -> Forms when
     Forms :: [erl_parse:abstract_form() | erl_parse:form_info()].
 parse_transform(Forms, _Options) ->
-    apply_transform(fun transform_do/1, Forms).
+    [erl_syntax:revert(erl_syntax_lib:map(fun transform_do/1, Form)) || Form <- Forms].
 
-transform_do({call, Line, {atom, Line, do}, Args}) ->
-    build_do_statement(Line, Args);
-transform_do({call, Line, {remote, Line, {atom, Line, ff_pipeline}, {atom, Line, do}}, Args}) ->
-    build_do_statement(Line, Args);
-transform_do(Node) ->
-    Node.
+transform_do(Tree) ->
+    TreePos = erl_syntax:get_pos(Tree),
+    case Tree of
+        ?Q("do(_@Tag, fun() -> _@@Body end)") ->
+            build_do_statement(TreePos, Tag, Body);
+        ?Q("ff_pipeline:do(_@Tag, fun() -> _@@Body end)") ->
+            build_do_statement(TreePos, Tag, Body);
+        ?Q("do(fun() -> _@@Body end)") ->
+            build_do_statement(TreePos, Body);
+        ?Q("ff_pipeline:do(fun() -> _@@Body end)") ->
+            build_do_statement(TreePos, Body);
+        _ ->
+            Tree
+    end.
 
-build_do_statement(Line, [Tag, Fun]) ->
-    ThrownVar = build_var_name(<<"Thrown">>, Line),
-    Error = {tuple, Line, [{atom, Line, error}, {tuple, Line, [Tag, {var, Line, ThrownVar}]}]},
-    build_do_statement(Line, ThrownVar, Error, Fun);
-build_do_statement(Line, [Fun]) ->
-    ThrownVar = build_var_name(<<"Thrown">>, Line),
-    Error = {tuple, Line, [{atom, Line, error}, {var, Line, ThrownVar}]},
-    build_do_statement(Line, ThrownVar, Error, Fun).
+build_do_statement(Pos, Body) ->
+    ThrownVar = build_var(<<"Thrown">>, Pos),
+    Error = ?Q("_@ThrownVar"),
+    build_do_statement(Pos, ThrownVar, Error, Body).
 
-build_do_statement(Line, ThrownVar, Error, Fun) ->
-    {'fun', _, {clauses, [{clause, _, [], [], Content}]}} = Fun,
-    NewContent = [
-        {call, Line, {remote, Line, {atom, Line, ff_pipeline}, {atom, Line, wrap}}, [
-            {block, Line, Content}
-        ]}
-    ],
-    {'try', Line,
-        NewContent,
-        [],
-        [{clause, Line,
-            [{tuple, Line, [{atom, Line, throw}, {var, Line, ThrownVar}, {var, Line, '_'}]}],
-            [],
-            [Error]}],
-        []}.
+build_do_statement(Pos, Tag, Body) ->
+    ThrownVar = build_var(<<"Thrown">>, Pos),
+    Error = ?Q("{_@Tag, _@ThrownVar}"),
+    build_do_statement(Pos, ThrownVar, Error, Body).
 
-build_var_name(Name, Line) ->
+build_do_statement(_Pos, ThrownVar, Error, Body) ->
+    ?Q([
+        "try ff_pipeline:wrap(begin _@@Body end)",
+        "catch _@ThrownVar -> {error, _@Error} end"
+    ]).
+
+build_var(Name, Line) ->
     LineStr = erlang:integer_to_binary(Line),
-    erlang:binary_to_atom(<<"$Pipeline", Name/binary, "-", LineStr/binary>>, latin1).
-
-%% AST traversal
-apply_transform(F, Node) ->
-    NewNode = F(Node),
-    do_apply_transform(F, NewNode).
-
-do_apply_transform(F, Nodes) when is_list(Nodes) ->
-    [apply_transform(F, N) || N <- Nodes];
-do_apply_transform(F, {function, Line, Name, Arity, Clauses}) ->
-    {function, Line, Name, Arity, apply_transform(F, Clauses)};
-do_apply_transform(F, {clause, Line, Head, Guard, Body}) ->
-    {clause, Line, Head, Guard, apply_transform(F, Body)};
-do_apply_transform(F, {block, Line, Es}) ->
-    {block, Line, apply_transform(F, Es)};
-do_apply_transform(F, {'if', Line, Cs}) ->
-    {'if', Line, apply_transform(F, Cs)};
-do_apply_transform(F, {'case', Line, E, Cs}) ->
-    {'case', Line, apply_transform(F, E), apply_transform(F, Cs)};
-do_apply_transform(F, {'receive', Line, Cs}) ->
-    {'receive', Line, apply_transform(F, Cs)};
-do_apply_transform(F, {'receive', Line, Cs, To, ToEs}) ->
-    {'receive', Line, apply_transform(F, Cs), apply_transform(F, To), apply_transform(F, ToEs)};
-do_apply_transform(F, {'try', Line, Es, Scs, Ccs, As}) ->
-    {'try', Line, apply_transform(F, Es), apply_transform(F, Scs), apply_transform(F, Ccs), apply_transform(F, As)};
-do_apply_transform(F, {'fun', Line, Body}) ->
-    {'fun', Line, apply_transform(F, Body)};
-do_apply_transform(F, {'catch', Line, E}) ->
-    {'catch', Line, apply_transform(F, E)};
-do_apply_transform(F, {call, Line, FName, As}) ->
-    {call, Line, apply_transform(F, FName), apply_transform(F, As)};
-do_apply_transform(F, {match, Line, L, R}) ->
-    {match, Line, apply_transform(F, L), apply_transform(F, R)};
-do_apply_transform(F, {cons, Line, H, T}) ->
-    {cons, Line, apply_transform(F, H), apply_transform(F, T)};
-do_apply_transform(F, {tuple, Line, Ps}) ->
-    {tuple, Line, apply_transform(F, Ps)};
-do_apply_transform(F, {map, Line, Fields}) ->
-    {map, Line, apply_transform(F, Fields)};
-do_apply_transform(F, {map, Line, Expr, Fields}) ->
-    {map, Line, apply_transform(F, Expr), apply_transform(F, Fields)};
-do_apply_transform(F, {record, Line, Name, Pfs}) ->
-    {record, Line, Name, apply_transform(F, Pfs)};
-do_apply_transform(F, {op, Line, Op, A}) ->
-    {op, Line, Op, apply_transform(F, A)};
-do_apply_transform(F, {op, Line, Op, L, R}) ->
-    {op, Line, Op, apply_transform(F, L), apply_transform(F, R)};
-do_apply_transform(F, {record_field, Line, Name, P}) ->
-    {record_field, Line, Name, apply_transform(F, P)};
-do_apply_transform(_F, Node) ->
-    Node.
+    VarName = erlang:binary_to_atom(<<"$Pipeline", Name/binary, "-", LineStr/binary>>, latin1),
+    merl:var(VarName).

cf18e94465d3f544c61803c2e09b841fb686b937
Артем <WWW_cool@inbox.ru>
2018-12-13 11:29:18 +0300

    FF-23: Woody retries (#44)

--- apps/ff_cth/src/ct_helper.erl
+++ apps/ff_cth/src/ct_helper.erl
@@ -112,6 +112,15 @@ start_app(wapi = AppName) ->
         {service_urls, #{
             cds_storage         => "http://cds:8022/v1/storage",
             identdoc_storage    => "http://cds:8022/v1/identity_document_storage"
+        }},
+        {service_retries, #{
+            fistful_stat    => #{
+                'GetWallets'   => {linear, 3, 1000},
+                '_'            => finish
+            }
+        }},
+        {api_deadlines, #{
+            fistful_stat => 5000
         }}
     ]), #{}};
 

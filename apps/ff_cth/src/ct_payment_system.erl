c180f82c86b022fd77295090ed0b241a73c62370
0x42 <bpozdnyakov@gmail.com>
2019-02-21 13:56:08 +0300

    FF-48: del lager error

--- apps/ff_cth/src/ct_payment_system.erl
+++ apps/ff_cth/src/ct_payment_system.erl
@@ -70,16 +70,7 @@ start_processing_apps(Options) ->
         client => ff_woody_client:new(<<"http://machinegun:8022/v1/automaton">>)
     }},
     {StartedApps, _StartupCtx} = ct_helper:start_apps([
-        % lager,
-        {lager, [
-            {error_logger_hwm, 600},
-            {crash_log, "crash.log"},
-            {handlers, [
-                {lager_console_backend, [
-                    {level, warning}
-                ]}
-            ]}
-        ]},
+        lager,
         scoper,
         woody,
         dmt_client,

d61fdd1a2cc9251f5744dad8f67ae12ad5d247e6
Evgeny Levenets <levenets.e.e@gmail.com>
2019-03-19 19:49:37 +0300

    lets brake tests first
    test-driven development FTW!

--- apps/ff_cth/src/ct_payment_system.erl
+++ apps/ff_cth/src/ct_payment_system.erl
@@ -512,8 +512,24 @@ default_termset(Options) ->
                     }
                 ]},
                 cash_flow = {decisions, [
+                    % this is impossible cash flow decision to check
+                    % if withdrawals cash flow calculates properly
                     #domain_CashFlowDecision{
-                        if_   = {condition, {currency_is, ?cur(<<"RUB">>)}},
+                        if_   = {
+                            condition,
+                            {payment_tool, {payment_terminal, #domain_PaymentTerminalCondition{}}}
+                        },
+                        then_ = {value, []}
+                    },
+                    #domain_CashFlowDecision{
+                        if_   = {all_of, ?ordset([
+                            {condition, {currency_is, ?cur(<<"RUB">>)}},
+                            {condition, {payment_tool, {bank_card, #domain_BankCardCondition{
+                                definition = {payment_system, #domain_PaymentSystemCondition{
+                                    payment_system_is = visa
+                                }}
+                            }}}}
+                        ])},
                         then_ = {value, [
                             ?cfpost(
                                 {wallet, sender_settlement},

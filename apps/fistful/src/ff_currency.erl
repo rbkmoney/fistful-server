9ac5a94c55476400f425703192d48e8211b1b1dc
Sergey Yelin <elinsn@gmail.com>
2019-10-03 13:33:01 +0300

    Use standard function for conversion

--- apps/fistful/src/ff_currency.erl
+++ apps/fistful/src/ff_currency.erl
@@ -25,7 +25,6 @@
 -export([get/1]).
 -export([symcode/1]).
 -export([id/1]).
--export([to_domain_ref/1]).
 
 %% Pipeline
 
@@ -58,10 +57,3 @@ get(ID) ->
             exponent => Currency#domain_Currency.exponent
         }
     end).
-
--spec to_domain_ref(currency()) -> dmsl_domain_thrift:'CurrencyRef'().
-
-to_domain_ref(Currency) ->
-    #domain_CurrencyRef{
-        symbolic_code = ff_currency:id(Currency)
-    }.

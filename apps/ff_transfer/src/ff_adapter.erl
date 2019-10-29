9559ad794186bc72aced9813ee9adc9f8fe63982
Roman Pushkov <pushkovroman@me.com>
2019-10-29 12:49:08 +0300

    FF-119: P2P adapter (#131)
    * add p2p_adapter implementation
    
    * some updates
    
    * fix service
    
    * add tests for adapter thrift interface
    
    * update p2p adapter interface
    
    * add app.src placeholder
    
    * add p2p adapter test handler
    
    * update p2p adapter tests
    
    * implement p2p adapter
    
    * update damsel
    
    * rename p2p to p2p_adapter
    
    * add transaction_info type to ff_adapter
    
    * update p2p adapter test
    
    * refactoring, change tuples to maps

--- apps/ff_transfer/src/ff_adapter.erl
+++ apps/ff_transfer/src/ff_adapter.erl
@@ -18,6 +18,29 @@
 
 -type opts() :: #{binary() => binary()}.
 
+-type transaction_info() :: #{
+    id              := binary(),
+    extra           := #{binary() => binary()},
+    timestamp       => binary(),
+    additional_info => additional_trx_info()
+}.
+
+-type additional_trx_info()         :: #{
+    rrn                   => binary(),
+    approval_code         => binary(),
+    acs_url               => binary(),
+    pareq                 => binary(),
+    md                    => binary(),
+    term_url              => binary(),
+    pares                 => binary(),
+    eci                   => binary(),
+    cavv                  => binary(),
+    xid                   => binary(),
+    cavv_algorithm        => binary(),
+    three_ds_verification => binary()
+}.
+
 -export_type([adapter/0]).
 -export_type([state/0]).
 -export_type([opts/0]).
+-export_type([transaction_info/0]).

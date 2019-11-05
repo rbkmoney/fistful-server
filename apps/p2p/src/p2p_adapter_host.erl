e51db491251ace186b3929b1af73981a9a89c68a
Roman Pushkov <pushkovroman@me.com>
2019-11-05 17:50:05 +0300

    update adapter host

--- apps/p2p/src/p2p_adapter_host.erl
+++ apps/p2p/src/p2p_adapter_host.erl
@@ -11,25 +11,10 @@
 %% Exports
 
 -export([handle_function/3]).
--export([process_callback/2]).
-
--define(SERVICE, {dmsl_p2p_adapter_thrift, 'P2PAdapterHost'}).
 
 %% Types
 
--type adapter()                     :: ff_adapter:adapter().
--type callback_tag()                :: binary().
--type callback_payload()            :: binary().
--type callback()                    :: p2p_adapter:callback().
-
 -type p2p_process_callback_result() :: dmsl_p2p_adapter_thrift:'ProcessCallbackResult'().
-
--type process_callback_result()     :: {ok, process_callback_finished() | process_callback_succeeded()}.
-
--type process_callback_finished()   :: {finished, p2p_adapter:context()}.
--type process_callback_succeeded()  :: {succeeded, callback_response_payload()}.
-
--type callback_response_payload()   :: binary().
 
 %% Handler
 
@@ -41,12 +26,23 @@ handle_function(Func, Args, Opts) ->
 %% Implementation
 
 -spec handle_function_('ProcessCallback', woody:args(), woody:options()) ->
-    {ok, woody:result()} | no_return().
-% placeholder
+    {ok, p2p_process_callback_result()} | no_return().
 handle_function_('ProcessCallback', [Callback], _Opts) ->
     #{tag := Tag, payload := Payload} = p2p_adapter_codec:decode_callback(Callback),
     case p2p_session_machine:process_callback(Tag, Payload) of
-      {ok, _result} -> Result;
-        {error, notfound} ->
+        {ok, #{payload := Payload}} ->
+            CallbackResponse = #p2p_adapter_CallbackResponse{payload = Payload},
+            Result = #p2p_adapter_ProcessCallbackSucceeded{response = CallbackResponse},
+            {ok, {succeeded, Result}};
+        {error, {session_already_finished, ID}} ->
+            {ok, SessionState} = p2p_session_machine:get(ID),
+            Session = p2p_session_machine:session(SessionState),
+            TransferParams = p2p_session:transfer_params(Session),
+            AdapterState = p2p_session:adapter_state(Session),
+            {_Adapter, AdapterOpts} = p2p_session:adapter(Session),
+            Context = p2p_adapter_codec:encode_context(TransferParams, AdapterState, AdapterOpts),
+            Result = #p2p_adapter_ProcessCallbackFinished{response = Context},
+            {ok, {finished, Result}};
+        {error, {unknown_p2p_session, _ID}} ->
             woody_error:raise(business, #p2p_adapter_SessionNotFound{})
     end.

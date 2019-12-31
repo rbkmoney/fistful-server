46602a743f58db5de5abed0ef6fba08e9a427925
Igor Toporkov <i.toporkov@rbkmoney.com>
2019-12-31 11:41:15 +0300

    Upgrade uac

--- apps/wapi/src/wapi_wallet_ff_backend.erl
+++ apps/wapi/src/wapi_wallet_ff_backend.erl
@@ -625,7 +625,7 @@ encode_webhook_id(WebhookID) ->
     end.
 
 maybe_check_quote_token(Params = #{<<"quoteToken">> := QuoteToken}, Context) ->
-    {ok, {_, _, Data}} = uac_authorizer_jwt:verify(QuoteToken, #{}),
+    {ok, {_, _, Data}} = uac_authorizer_jwt:verify(QuoteToken, #{should_authorize_operations => false}),
     unwrap(quote_invalid_party,
         valid(
             maps:get(<<"partyID">>, Data),

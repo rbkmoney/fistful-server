28d588867bd3118fd4a6d9becfd05b1ad67e60f8
Kehitt <devstealth@gmail.com>
2020-04-09 15:33:03 +0300

    fix dialzyer

--- apps/wapi/src/wapi_swagger_validator.erl
+++ apps/wapi/src/wapi_swagger_validator.erl
@@ -3,8 +3,8 @@
 -type param_rule()   :: swag_server_wallet_param_validator:param_rule().
 -type schema_rule()  :: swag_server_wallet_schema_validator:schema_rule().
 -type value()        :: swag_server_wallet:value().
--type param_meta()   :: swag_server_wallet_param_validator:meta().
--type schema_meta()  :: swag_server_wallet_schema_validator:meta().
+-type param_context()   :: swag_server_wallet_param_validator:context().
+-type schema_context()  :: swag_server_wallet_schema_validator:context().
 
 -type validate_param_result() ::
     ok | {ok, term()} | pass | error | {error, Error :: term()}.
@@ -17,12 +17,12 @@
 -export([validate_param/3]).
 -export([validate_schema/4]).
 
--spec validate_param(param_rule(), value(), param_meta()) ->
+-spec validate_param(param_rule(), value(), param_context()) ->
     validate_param_result().
 validate_param(_Rule, _Value, _Meta) ->
     pass.
 
--spec validate_schema(schema_rule(), value(), schema_meta(), jesse_state:state()) ->
+-spec validate_schema(schema_rule(), value(), schema_context(), jesse_state:state()) ->
     validate_schema_result().
 
 validate_schema(

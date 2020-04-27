4291900035862a6448d67e46fbde5de347c7ecff
Andrey Fadeev <me@ciiol.net>
2020-04-27 13:23:36 +0300

    Fix `add_events` repair scenario action decoding (#209)
    Type `machinery:result/2` has field `action`, not `actions`

--- apps/ff_server/src/ff_wallet_codec.erl
+++ apps/ff_server/src/ff_wallet_codec.erl
@@ -77,7 +77,7 @@ unmarshal({list, T}, V) ->
 unmarshal(repair_scenario, {add_events, #wlt_AddEventsRepair{events = Events, action = Action}}) ->
     {add_events, genlib_map:compact(#{
         events => unmarshal({list, change}, Events),
-        actions => maybe_unmarshal(complex_action, Action)
+        action => maybe_unmarshal(complex_action, Action)
     })};
 
 unmarshal(change, {created, Wallet}) ->

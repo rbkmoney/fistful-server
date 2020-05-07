9052da1d1bd019d36444734abb74795b0a87726d
Igor Toporkov <i.toporkov@rbkmoney.com>
2020-05-07 15:30:01 +0300

    WIP: Attempt to do withdrawal migrations in corresponding schema module

--- apps/fistful/src/ff_withdrawal_machinery_mg_schema.erl
+++ apps/fistful/src/ff_withdrawal_machinery_mg_schema.erl
@@ -10,7 +10,7 @@
 -export([unmarshal/2]).
 
 -define(CONTENT_TYPE, {struct, struct, {mg_proto_state_processing_thrift, 'Content'}}).
--define(VERSION, 1).
+-define(VERSION, 2).
 
 -type t()  :: machinery_mg_schema:t().
 -type v(T) :: machinery_mg_schema:v(T).
@@ -39,16 +39,10 @@ marshal(T, V) ->
 unmarshal(event, V) ->
     Type = {struct, union, {ff_proto_withdrawal_thrift, 'NoIdEvent'}},
     {TimeStamp, Change} = unwrap(Type, V),
-    {ev, ff_codec:unmarshal(timestamp, TimeStamp), add_version(ff_withdrawal_codec:unmarshal(change, Change), 2)};
+    {ev, ff_codec:unmarshal(timestamp, TimeStamp), ff_withdrawal_codec:unmarshal(change, Change)};
 
 unmarshal(T, V) ->
     machinery_mg_schema_generic:unmarshal(T, V).
-
-% TODO: migrations
-add_version({created, Withdrawal}, Version) ->
-    {created, Withdrawal#{version => Version}};
-add_version(Change, _) ->
-    Change.
 
 wrap(Type, Data) ->
     Content = #mg_stateproc_Content{
@@ -60,6 +54,201 @@ wrap(Type, Data) ->
 unwrap(Type, Bin) ->
     #mg_stateproc_Content{
         data = Data,
-        format_version = _Version
+        format_version = Version
     } = ff_proto_utils:deserialize(?CONTENT_TYPE, Bin),
-    ff_proto_utils:deserialize(Type, Data).
+    {TimeStamp, Change0} = ff_proto_utils:deserialize(Type, Data),
+    {TimeStamp, case Version of
+        ?VERSION ->
+            Change0;
+        _Other ->
+            maybe_migrate(Change0, #{timestamp => TimeStamp})
+    end}.
+
+
+%% Migrations
+
+maybe_migrate(Ev = {status_changed, {failed, #{code := _}}}, _MigrateParams) ->
+    Ev;
+maybe_migrate(Ev = {session_finished, {_SessionID, _Status}}, _MigrateParams) ->
+    Ev;
+maybe_migrate(Ev = {limit_check, {wallet_sender, _Details}}, _MigrateParams) ->
+    Ev;
+maybe_migrate({p_transfer, PEvent}, _MigrateParams) ->
+    {p_transfer, ff_postings_transfer:maybe_migrate(PEvent, withdrawal)};
+maybe_migrate({adjustment, _Payload} = Event, _MigrateParams) ->
+    ff_adjustment_utils:maybe_migrate(Event);
+maybe_migrate({resource_got, Resource}, _MigrateParams) ->
+    {resource_got, ff_instrument:maybe_migrate_resource(Resource)};
+
+% Old events
+maybe_migrate({limit_check, {wallet, Details}}, MigrateParams) ->
+    maybe_migrate({limit_check, {wallet_sender, Details}}, MigrateParams);
+maybe_migrate({created, #{version := 1, handler := ff_withdrawal} = T}, MigrateParams) ->
+    #{
+        version     := 1,
+        id          := ID,
+        handler     := ff_withdrawal,
+        body        := Body,
+        params      := #{
+            destination := DestinationID,
+            source      := SourceID
+        }
+    } = T,
+    Route = maps:get(route, T, undefined),
+    maybe_migrate({created, genlib_map:compact(#{
+        version       => 2,
+        id            => ID,
+        transfer_type => withdrawal,
+        body          => Body,
+        route         => Route,
+        params        => #{
+            wallet_id             => SourceID,
+            destination_id        => DestinationID,
+            % Fields below are required to correctly decode legacy events.
+            % When decoding legacy events, the `erlang:binary_to_existing_atom/2` function is used,
+            % so the code must contain atoms from the event.
+            % They are not used now, so their value does not matter.
+            wallet_account        => [],
+            destination_account   => [],
+            wallet_cash_flow_plan => []
+        }
+    })}, MigrateParams);
+maybe_migrate({created, T}, MigrateParams) ->
+    DestinationID = maps:get(destination, T),
+    SourceID = maps:get(source, T),
+    ProviderID = maps:get(provider, T),
+    maybe_migrate({created, T#{
+        version     => 1,
+        handler     => ff_withdrawal,
+        route       => #{provider_id => ProviderID},
+        params => #{
+            destination => DestinationID,
+            source      => SourceID
+        }
+    }}, MigrateParams);
+maybe_migrate({transfer, PTransferEv}, MigrateParams) ->
+    maybe_migrate({p_transfer, PTransferEv}, MigrateParams);
+maybe_migrate({status_changed, {failed, LegacyFailure}}, MigrateParams) ->
+    Failure = #{
+        code => <<"unknown">>,
+        reason => genlib:format(LegacyFailure)
+    },
+    maybe_migrate({status_changed, {failed, Failure}}, MigrateParams);
+maybe_migrate({session_finished, SessionID}, MigrateParams) ->
+    {ok, SessionMachine} = ff_withdrawal_session_machine:get(SessionID),
+    Session = ff_withdrawal_session_machine:session(SessionMachine),
+    {finished, Result} = ff_withdrawal_session:status(Session),
+    maybe_migrate({session_finished, {SessionID, Result}}, MigrateParams);
+% Other events
+maybe_migrate(Ev, _MigrateParams) ->
+    Ev.
+
+%% Tests
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-spec test() -> _.
+
+-spec v0_created_migration_test() -> _.
+v0_created_migration_test() ->
+    ID = genlib:unique(),
+    WalletID = genlib:unique(),
+    DestinationID = genlib:unique(),
+    ProviderID = genlib:unique(),
+    Body = {100, <<"RUB">>},
+    LegacyEvent = {created, #{
+        id          => ID,
+        source      => WalletID,
+        destination => DestinationID,
+        body        => Body,
+        provider    => ProviderID
+    }},
+    {created, Withdrawal} = maybe_migrate(LegacyEvent, #{}),
+    ?assertEqual(ID, id(Withdrawal)),
+    ?assertEqual(WalletID, wallet_id(Withdrawal)),
+    ?assertEqual(DestinationID, destination_id(Withdrawal)),
+    ?assertEqual(Body, body(Withdrawal)),
+    ?assertEqual(#{provider_id => ProviderID}, route(Withdrawal)).
+
+-spec v1_created_migration_test() -> _.
+v1_created_migration_test() ->
+    ID = genlib:unique(),
+    WalletID = genlib:unique(),
+    WalletAccount = #{
+        id => WalletID,
+        identity => genlib:unique(),
+        currency => <<"RUB">>,
+        accounter_account_id => 123
+    },
+    DestinationID = genlib:unique(),
+    DestinationAccount = #{
+        id => DestinationID,
+        identity => genlib:unique(),
+        currency => <<"RUB">>,
+        accounter_account_id => 123
+    },
+    Body = {100, <<"RUB">>},
+    LegacyEvent = {created, #{
+        version     => 1,
+        id          => ID,
+        handler     => ff_withdrawal,
+        source      => WalletAccount,
+        destination => DestinationAccount,
+        body        => Body,
+        params      => #{
+            source => WalletID,
+            destination => DestinationID
+        }
+    }},
+    {created, Withdrawal} = maybe_migrate(LegacyEvent, #{}),
+    ?assertEqual(ID, id(Withdrawal)),
+    ?assertEqual(WalletID, wallet_id(Withdrawal)),
+    ?assertEqual(DestinationID, destination_id(Withdrawal)),
+    ?assertEqual(Body, body(Withdrawal)).
+
+-spec v2_created_migration_test() -> _.
+v2_created_migration_test() ->
+    ID = genlib:unique(),
+    WalletID = genlib:unique(),
+    WalletAccount = #{
+        id => WalletID,
+        identity => genlib:unique(),
+        currency => <<"RUB">>,
+        accounter_account_id => 123
+    },
+    DestinationID = genlib:unique(),
+    DestinationAccount = #{
+        id => DestinationID,
+        identity => genlib:unique(),
+        currency => <<"RUB">>,
+        accounter_account_id => 123
+    },
+    Body = {100, <<"RUB">>},
+    LegacyEvent = {created, #{
+        version       => 2,
+        id            => ID,
+        transfer_type => withdrawal,
+        body          => Body,
+        params        => #{
+            wallet_id             => WalletID,
+            destination_id        => DestinationID,
+            wallet_account        => WalletAccount,
+            destination_account   => DestinationAccount,
+            wallet_cash_flow_plan => #{
+                postings => [
+                    #{
+                        sender   => {wallet, sender_settlement},
+                        receiver => {wallet, receiver_destination},
+                        volume   => {share, {{1, 1}, operation_amount, default}}
+                    }
+                ]
+            }
+        }
+    }},
+    {created, Withdrawal} = maybe_migrate(LegacyEvent, #{}),
+    ?assertEqual(ID, id(Withdrawal)),
+    ?assertEqual(WalletID, wallet_id(Withdrawal)),
+    ?assertEqual(DestinationID, destination_id(Withdrawal)),
+    ?assertEqual(Body, body(Withdrawal)).
+
+-endif.

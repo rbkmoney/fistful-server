c3bfb6933e8f3ffa1a10dda42c4f4948f60f7722
Igor Toporkov <i.toporkov@rbkmoney.com>
2020-05-07 11:44:33 +0300

    Use generic schema to marshal non-events

--- apps/fistful/src/ff_withdrawal_machinery_mg_schema.erl
+++ apps/fistful/src/ff_withdrawal_machinery_mg_schema.erl
@@ -24,60 +24,29 @@
 
 -spec marshal(t(), v(withdrawal_data())) ->
     machinery_msgpack:t().
-marshal(T, V) ->
-    do_marshal(T, V).
-
-do_marshal({args, init}, {Changes, Ctx}) ->
-    Type = {struct, union, {ff_proto_withdrawal_thrift, 'InitArgs'}},
-    ThriftChanges = lists:map(fun(Change) -> ff_withdrawal_codec:marshal(change, Change) end, Changes),
-    Data = #wthd_InitArgs{changes = ThriftChanges, context = ff_codec:marshal(context, Ctx)},
-    Version = get_withdrawal_version(Changes),
-    Bin = wrap(Type, Data, Version),
-    {bin, Bin};
-do_marshal(aux_state, #{ctx := AuxState}) ->
-    Type = {map, string, {struct, union, {ff_proto_msgpack_thrift, 'Value'}}},
-    Bin = wrap(Type, ff_codec:marshal(context, AuxState), undefined),
-    {bin, Bin};
-do_marshal(event, {ev, Ts, Change}) ->
+marshal(event, {ev, Ts, Change}) ->
     ThriftChange = ff_withdrawal_codec:marshal(change, Change),
     TimeStamp = ff_codec:marshal(timestamp, Ts),
     Data = #wthd_NoIdEvent{change = ThriftChange, occured_at = TimeStamp},
     Type = {struct, union, {ff_proto_withdrawal_thrift, 'NoIdEvent'}},
     Version = get_withdrawal_version(Change),
     Bin = wrap(Type, Data, Version),
-    {bin, Bin}.
+    {bin, Bin};
+marshal(T, V) ->
+    machinery_mg_schema_generic:marshal(T, V).
 
 -spec unmarshal(t(), machinery_msgpack:t()) ->
     withdrawal_data().
-unmarshal(T, V) ->
-    do_unmarshal(T, V).
-
-do_unmarshal({args, init}, V) ->
-    Type = {struct, struct, {ff_proto_withdrawal_thrift, 'InitArgs'}},
-    #{
-        data := #wthd_InitArgs{
-            changes = ThriftChanges,
-            context = ThriftContext
-        },
-        version := Version
-    } =  unwrap(Type, V),
-    Changes0 = lists:map(fun(Change) -> ff_withdrawal_codec:unmarshal(change, Change) end, ThriftChanges),
-    Changes = add_version(Changes0, Version),
-    {Changes, ff_codec:unmarshal(context, ThriftContext)};
-do_unmarshal(aux_state, {bin, <<>>}) -> % TODO: discover where it comes from
-    #{ctx => #{}};
-do_unmarshal(aux_state, V) ->
-    Type = {map, string, {struct, union, {ff_proto_msgpack_thrift, 'Value'}}},
-    #{data := Aux} = unwrap(Type, V),
-    #{ctx => ff_codec:unmarshal(context, Aux)};
-do_unmarshal(event, V) ->
+unmarshal(event, V) ->
     Type = {struct, union, {ff_proto_withdrawal_thrift, 'NoIdEvent'}},
     #{
         data := {TimeStamp, Change},
         version := Version
     } = unwrap(Type, V),
-    {ev, ff_codec:unmarshal(timestamp, TimeStamp), add_version(ff_withdrawal_codec:unmarshal(change, Change), Version)}.
+    {ev, ff_codec:unmarshal(timestamp, TimeStamp), add_version(ff_withdrawal_codec:unmarshal(change, Change), Version)};
 
+unmarshal(T, V) ->
+    machinery_mg_schema_generic:unmarshal(T, V).
 
 add_version(Changes, Version) when is_list(Changes) ->
     F = fun(Change) when is_tuple(Change) -> add_version(Change, Version) end,

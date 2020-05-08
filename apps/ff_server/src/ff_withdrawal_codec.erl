a985f686fdfee39e2e40bb49e3fcf594c23b45b2
Igor Toporkov <i.toporkov@rbkmoney.com>
2020-05-08 15:01:00 +0300

    Fix failing route_change unmarshaling

--- apps/ff_server/src/ff_withdrawal_codec.erl
+++ apps/ff_server/src/ff_withdrawal_codec.erl
@@ -186,7 +186,7 @@ unmarshal(change, {transfer, #wthd_TransferChange{payload = TransferChange}}) ->
     {p_transfer, ff_p_transfer_codec:unmarshal(change, TransferChange)};
 unmarshal(change, {session, SessionChange}) ->
     unmarshal(session_event, SessionChange);
-unmarshal(change, {route, Route}) ->
+unmarshal(change, {route, #wthd_RouteChange{route = Route}}) ->
     {route_changed, unmarshal(route, Route)};
 unmarshal(change, {limit_check, #wthd_LimitCheckChange{details = Details}}) ->
     {limit_check, ff_limit_check_codec:unmarshal(details, Details)};

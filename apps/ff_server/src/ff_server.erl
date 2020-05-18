97c0d55dde6e5be963a261373a89d48df812ade9
WWW_cool <WWW_cool@inbox.ru>
2020-05-18 14:25:07 +0300

    added p2p template tests

--- apps/ff_server/src/ff_server.erl
+++ apps/ff_server/src/ff_server.erl
@@ -102,7 +102,8 @@ init([]) ->
         {withdrawal_repairer, ff_withdrawal_repair},
         {deposit_repairer, ff_deposit_repair},
         {p2p_transfer_repairer, ff_p2p_transfer_repair},
-        {p2p_session_repairer, ff_p2p_session_repair}
+        {p2p_session_repairer, ff_p2p_session_repair},
+        {p2p_template_management, ff_p2p_template_handler}
     ] ++ get_eventsink_handlers(),
     WoodyHandlers = [get_handler(Service, Handler, WrapperOpts) || {Service, Handler} <- Services],
 

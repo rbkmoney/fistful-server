6d6943375fb2fb2f8abe4b167d93bea21d5440e9
Roman Pushkov <pushkovroman@me.com>
2020-06-05 18:21:28 +0300

    add graceful shutdown tests

---
+++ apps/wapi/test/wapi_graceful_shutdown_SUITE.erl
@@ -0,0 +1,242 @@
+-module(wapi_graceful_shutdown_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+
+-include_lib("damsel/include/dmsl_domain_config_thrift.hrl").
+
+-include_lib("jose/include/jose_jwk.hrl").
+-include_lib("wapi_wallet_dummy_data.hrl").
+
+-include_lib("fistful_proto/include/ff_proto_wallet_thrift.hrl").
+
+-export([all/0]).
+-export([groups/0]).
+-export([init_per_suite/1]).
+-export([end_per_suite/1]).
+-export([init_per_group/2]).
+-export([end_per_group/2]).
+-export([init_per_testcase/2]).
+-export([end_per_testcase/2]).
+
+-export([init/1]).
+
+-export([
+    shutdown_test/1,
+    request_interrupt_test/1
+]).
+
+% common-api is used since it is the domain used in production RN
+% TODO: change to wallet-api (or just omit since it is the default one) when new tokens will be a thing
+-define(DOMAIN, <<"common-api">>).
+-define(badresp(Code), {error, {invalid_response_code, Code}}).
+-define(emptyresp(Code), {error, {Code, #{}}}).
+-define(NUMBER_OF_WORKERS, 10).
+
+-type test_case_name()  :: atom().
+-type config()          :: [{atom(), any()}].
+-type group_name()      :: atom().
+
+-behaviour(supervisor).
+
+-spec init([]) ->
+    {ok, {supervisor:sup_flags(), [supervisor:child_spec()]}}.
+init([]) ->
+    {ok, {#{strategy => one_for_all, intensity => 1, period => 1}, []}}.
+
+-spec all() ->
+    [test_case_name()].
+all() ->
+    [
+        {group, base}
+    ].
+
+-spec groups() ->
+    [{group_name(), list(), [test_case_name()]}].
+groups() ->
+    [
+        {base, [],
+            [
+                shutdown_test,
+                request_interrupt_test
+            ]
+        }
+    ].
+
+%%
+%% starting/stopping
+%%
+-spec init_per_suite(config()) ->
+    config().
+init_per_suite(Config) ->
+    %% TODO remove this after cut off wapi
+    ok = application:set_env(wapi, transport, thrift),
+    ct_helper:makeup_cfg([
+        ct_helper:test_case_name(init),
+        ct_payment_system:setup(#{
+            optional_apps => [
+                bender_client,
+                wapi_woody_client,
+                wapi
+            ]
+        })
+    ], Config).
+
+-spec end_per_suite(config()) ->
+    _.
+end_per_suite(C) ->
+    %% TODO remove this after cut off wapi
+    ok = application:unset_env(wapi, transport),
+    ok = ct_payment_system:shutdown(C).
+
+-spec init_per_group(group_name(), config()) ->
+    config().
+init_per_group(Group, Config) when Group =:= base ->
+    ok = ff_context:save(ff_context:create(#{
+        party_client => party_client:create_client(),
+        woody_context => woody_context:new(<<"init_per_group/", (atom_to_binary(Group, utf8))/binary>>)
+    })),
+    Party = create_party(Config),
+    BasePermissions = [
+        {[party], read},
+        {[party], write}
+    ],
+    {ok, Token} = wapi_ct_helper:issue_token(Party, BasePermissions, {deadline, 10}, ?DOMAIN),
+    Config1 = [{party, Party} | Config],
+    [{context, wapi_ct_helper:get_context(Token)} | Config1];
+init_per_group(_, Config) ->
+    Config.
+
+-spec end_per_group(group_name(), config()) ->
+    _.
+end_per_group(_Group, _C) ->
+    ok.
+
+-spec init_per_testcase(test_case_name(), config()) ->
+    config().
+init_per_testcase(Name, C) ->
+    C1 = ct_helper:makeup_cfg([ct_helper:test_case_name(Name), ct_helper:woody_ctx()], C),
+    ok = ct_helper:set_context(C1),
+    [{test_sup, wapi_ct_helper:start_mocked_service_sup(?MODULE)} | C1].
+
+-spec end_per_testcase(test_case_name(), config()) ->
+    config().
+end_per_testcase(_Name, C) ->
+    ok = ct_helper:unset_context(),
+    _ = application:start(wapi),
+    wapi_ct_helper:stop_mocked_service_sup(?config(test_sup, C)),
+    ok.
+
+%%% Tests
+
+-spec shutdown_test(config()) ->
+    _.
+shutdown_test(C) ->
+    PartyID = ?config(party, C),
+    Context = ct_helper:cfg(context, C),
+    wapi_ct_helper:mock_services([
+        {fistful_identity, fun('Get', _) -> ok = timer:sleep(2000), {ok, ?IDENTITY(PartyID)} end},
+        {fistful_wallet, fun('Create', _) -> {ok, ?WALLET(PartyID)} end}
+    ], C),
+    {ok, _} = call_api(
+        fun swag_client_wallet_wallets_api:create_wallet/3,
+        #{
+            body => #{
+                <<"name">> => ?STRING,
+                <<"identity">> => ?STRING,
+                <<"currency">> => ?RUB,
+                <<"metadata">> => #{
+                    <<"somedata">> => ?STRING
+                }
+            }
+        },
+        Context
+    ),
+    ok = spawn_workers(Context, self(), ?NUMBER_OF_WORKERS),
+    ok = timer:sleep(1000),
+    ok = application:stop(wapi),
+    ok = receive_loop(fun(Result) -> {ok, _} = Result end, ?NUMBER_OF_WORKERS, timer:seconds(20)),
+    ok = spawn_workers(Context, self(), ?NUMBER_OF_WORKERS),
+    ok = receive_loop(fun(Result) -> {error, econnrefused} = Result end, ?NUMBER_OF_WORKERS, timer:seconds(20)).
+
+-spec request_interrupt_test(config()) ->
+    _.
+request_interrupt_test(C) ->
+    PartyID = ?config(party, C),
+    Context = ct_helper:cfg(context, C),
+    wapi_ct_helper:mock_services([
+        {fistful_identity, fun('Get', _) -> ok = timer:sleep(2000), {ok, ?IDENTITY(PartyID)} end},
+        {fistful_wallet, fun('Create', _) -> {ok, ?WALLET(PartyID)} end}
+    ], C),
+    {ok, _} = call_api(
+        fun swag_client_wallet_wallets_api:create_wallet/3,
+        #{
+            body => #{
+                <<"name">> => ?STRING,
+                <<"identity">> => ?STRING,
+                <<"currency">> => ?RUB,
+                <<"metadata">> => #{
+                    <<"somedata">> => ?STRING
+                }
+            }
+        },
+        Context
+    ),
+    ok = spawn_workers(Context, self(), ?NUMBER_OF_WORKERS),
+    ok = timer:sleep(1000),
+    ok = application:stop(wapi),
+    ok = receive_loop(fun(Result) -> {ok, _} = Result end, ?NUMBER_OF_WORKERS, timer:seconds(20)),
+    ok = spawn_workers(Context, self(), ?NUMBER_OF_WORKERS),
+    ok = receive_loop(fun(Result) -> {error, econnrefused} = Result end, ?NUMBER_OF_WORKERS, timer:seconds(20)).
+
+%%
+
+receive_loop(_, N, _Timeout) when N =< 0 ->
+    ok;
+receive_loop(MatchFun, N, Timeout) ->
+    receive
+        {result, Result} ->
+            MatchFun(Result)
+    after Timeout ->
+        error(timeout)
+    end,
+    receive_loop(MatchFun, N - 1, Timeout).
+
+spawn_workers(_, _, N) when N =< 0 ->
+    ok;
+spawn_workers(Context, ParentPID, N) ->
+    erlang:spawn_link(fun() -> worker(Context, ParentPID) end),
+    spawn_workers(Context, ParentPID, N - 1).
+
+worker(Context, ParentPID) ->
+    Result = call_api(
+        fun swag_client_wallet_wallets_api:create_wallet/3,
+        #{
+            body => #{
+                <<"name">> => ?STRING,
+                <<"identity">> => ?STRING,
+                <<"currency">> => ?RUB,
+                <<"metadata">> => #{
+                    <<"somedata">> => ?STRING
+                }
+            }
+        },
+        Context
+    ),
+    ParentPID ! {result, Result}.
+
+% build_deadline(CurrentSeconds) ->
+%     genlib_rfc3339:format_relaxed(genlib_time:add_hours(CurrentSeconds, 1), second).
+
+%%
+
+-spec call_api(function(), map(), wapi_client_lib:context()) ->
+    {ok, term()} | {error, term()}.
+call_api(F, Params, Context) ->
+    {Url, PreparedParams, Opts} = wapi_client_lib:make_request(Context, Params),
+    Response = F(Url, PreparedParams, Opts),
+    wapi_client_lib:handle_response(Response).
+
+create_party(_C) ->
+    ID = genlib:bsuuid(),
+    _ = ff_party:create(ID),
+    ID.

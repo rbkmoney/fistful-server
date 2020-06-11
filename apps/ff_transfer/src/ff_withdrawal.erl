5751b2005cbdd5a2bf352bbc864d3efcbc47aa38
Sergey Yelin <elinsn@gmail.com>
2020-06-11 16:28:28 +0300

    P2C-4: fix crash (#230)
    * Add provider selection
    
    * P2C-4: Add routing
    
    * Fix test
    
    * Withdrawal session is actually list
    
    * Fixes
    
    * Typo in spec
    
    * Fix another typo in spec
    
    * Hide route session from withdrawal
    
    * Simplify session_processing_status/1
    
    * Match sessions and transfers
    
    In current design withdrawal has one session with corresponding transfer
    per route. So there is no need to have explicit counter.
    
    * Rework routing, move it to own module
    
    * Use counter for sessions and transaction cound
    
    * Recalculate provider on route_change event
    
    * Add quote test for routing
    
    * Fix
    
    * Separate ff_withdrawal and ff_withdrawal_route_utils
    
    * choose_provider -> filter_providers
    
    * Rename routes() to more sutable attempts()
    
    * Don not record transient error
    
    * Remove unreachable error handling here
    
    * Use route() as for attempts
    
    * Review fixes
    
    * Lazy init for attempts
    
    * Fix crash on marshalling withdrawal without attempts
    
    * Fix crash on "new" withdrawal id format (which is actually session_id)
    
    * Make session_id optional
    
    * Fix type
    
    * typo
    
    * Fix test

--- apps/ff_transfer/src/ff_withdrawal.erl
+++ apps/ff_transfer/src/ff_withdrawal.erl
@@ -836,6 +836,7 @@ process_session_creation(Withdrawal) ->
         quote       => unwrap_quote(quote(Withdrawal))
     }),
     SessionParams = #{
+        withdrawal_id => id(Withdrawal),
         resource => destination_resource(Withdrawal),
         provider_id => ProviderID
     },

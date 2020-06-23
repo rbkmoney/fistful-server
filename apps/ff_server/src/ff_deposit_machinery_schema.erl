5715f0ce8b0260072f38e86ad17ed135bf12f4f9
r.shaidullin <ndiezel0@gmail.com>
2020-06-23 14:54:32 +0300

    FF-187: Add tests

--- apps/ff_server/src/ff_deposit_machinery_schema.erl
+++ apps/ff_server/src/ff_deposit_machinery_schema.erl
@@ -92,16 +92,78 @@ unmarshal_event(1, EncodedChange, Context) ->
 unmarshal_event(undefined = Version, EncodedChange, Context0) ->
     {Event, Context1} = machinery_mg_schema_generic:unmarshal({event, Version}, EncodedChange, Context0),
     {ev, Timestamp, Change} = Event,
-    {{ev, Timestamp, maybe_migrate(Change)}, Context1}.
+    {{ev, Timestamp, maybe_migrate(Change, Context1)}, Context1}.
 
--spec maybe_migrate(any()) ->
+-spec maybe_migrate(any(), ff_machine:migrate_params()) ->
     ff_deposit:event().
-maybe_migrate({created, #{version := 3}} = Event) ->
-    Event;
-maybe_migrate({created, Deposit}) ->
-    {created, Deposit#{version => 3}};
-% Other events
-maybe_migrate(Ev) ->
+maybe_migrate(Ev = {status_changed, {failed, #{code := _}}}, _MigrateParams) ->
+    Ev;
+maybe_migrate(Ev = {limit_check, {wallet_receiver, _Details}}, _MigrateParams) ->
+    Ev;
+maybe_migrate({p_transfer, PEvent}, _MigrateParams) ->
+    {p_transfer, ff_postings_transfer:maybe_migrate(PEvent, deposit)};
+maybe_migrate({revert, _Payload} = Event, _MigrateParams) ->
+    ff_deposit_revert_utils:maybe_migrate(Event);
+maybe_migrate({adjustment, _Payload} = Event, _MigrateParams) ->
+    ff_adjustment_utils:maybe_migrate(Event);
+
+% Old events
+maybe_migrate({limit_check, {wallet, Details}}, MigrateParams) ->
+    maybe_migrate({limit_check, {wallet_receiver, Details}}, MigrateParams);
+maybe_migrate({created, #{version := 1, handler := ff_deposit} = T}, MigrateParams) ->
+    #{
+        version     := 1,
+        id          := ID,
+        handler     := ff_deposit,
+        source      := _SourceAccount,
+        destination := _DestinationAccount,
+        body        := Body,
+        params      := #{
+            destination := DestinationID,
+            source      := SourceID
+        }
+    } = T,
+    maybe_migrate({created, #{
+        version       => 2,
+        id            => ID,
+        transfer_type => deposit,
+        body          => Body,
+        params        => #{
+            wallet_id             => DestinationID,
+            source_id             => SourceID,
+            % Fields below are required to correctly decode legacy events.
+            % When decoding legacy events, the `erlang:binary_to_existing_atom/2` function is used,
+            % so the code must contain atoms from the event.
+            % They are not used now, so their value does not matter.
+            wallet_account        => [],
+            source_account        => [],
+            wallet_cash_flow_plan => []
+        }
+    }}, MigrateParams);
+maybe_migrate({created, Deposit = #{version := 2, id := ID}}, MigrateParams) ->
+    Ctx = maps:get(ctx, MigrateParams, undefined),
+    Context = case Ctx of
+                  undefined ->
+                      {ok, State} = ff_machine:get(ff_withdrawal, 'ff/withdrawal_v2', ID, {undefined, 0, forward}),
+                      maps:get(ctx, State, undefined);
+                  Data ->
+                      Data
+              end,
+    maybe_migrate({created, genlib_map:compact(Deposit#{
+        version => 3,
+        metadata => ff_entity_context:try_get_legacy_metadata(Context)
+    })}, MigrateParams);
+maybe_migrate({created, #{version := 3}} = Ev, _MigrateParams) ->
+    Ev;
+maybe_migrate({transfer, PTransferEv}, MigrateParams) ->
+    maybe_migrate({p_transfer, PTransferEv}, MigrateParams);
+maybe_migrate({status_changed, {failed, LegacyFailure}}, MigrateParams) ->
+    Failure = #{
+        code => <<"unknown">>,
+        reason => genlib:format(LegacyFailure)
+    },
+    maybe_migrate({status_changed, {failed, Failure}}, MigrateParams);
+maybe_migrate(Ev, _MigrateParams) ->
     Ev.
 
 
@@ -137,7 +199,8 @@ created_v0_3_decoding_test() ->
         params => #{
             wallet_id => <<"wallet_id">>,
             source_id => <<"source_id">>
-        }
+        },
+        metadata => #{}
     },
     Change = {created, Deposit},
     Event = {ev, {{{2020, 5, 25}, {19, 19, 10}}, 293305}, Change},
@@ -158,7 +221,8 @@ created_v0_3_decoding_test() ->
                 {str, <<"params">>} => {arr, [{str, <<"map">>}, {obj, #{
                     {str, <<"wallet_id">>} => {bin, <<"wallet_id">>},
                     {str, <<"source_id">>} => {bin, <<"source_id">>}
-                }}]}
+                }}]},
+                {str, <<"metadata">>} => {arr, [{str, <<"map">>}, {obj, #{}}]}
             }}
         ]}
     ]},
@@ -177,6 +241,126 @@ created_v0_3_decoding_test() ->
         LegacyChange
     ]},
     DecodedLegacy = unmarshal({event, undefined}, LegacyEvent),
+    ModernizedBinary = marshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, DecodedLegacy),
+    Decoded = unmarshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, ModernizedBinary),
+    ?assertEqual(Event, Decoded).
+
+-spec created_v1_3_decoding_test() -> _.
+created_v1_3_decoding_test() ->
+    Deposit = #{
+        version => 3,
+        id => <<"deposit">>,
+        body => {123, <<"RUB">>},
+        params => #{
+            wallet_id => <<"wallet_id">>,
+            source_id => <<"source_id">>
+        },
+        metadata => #{}
+    },
+    Change = {created, Deposit},
+    Event = {ev, {{{2020, 5, 25}, {19, 19, 10}}, 293305}, Change},
+
+    LegacyAccount = {arr, [{str, <<"map">>}, {obj, #{
+        {str, <<"id">>} => {bin, <<"id">>},
+        {str, <<"identity">>} => {bin, <<"id">>},
+        {str, <<"currency">>} => {bin, <<"id">>},
+        {str, <<"accounter_account_id">>} => {bin, <<"id">>}
+        }}]},
+    LegacyChange = {arr, [
+        {str, <<"tup">>},
+        {str, <<"created">>},
+        {arr, [
+            {str, <<"map">>},
+            {obj, #{
+                {str, <<"version">>} => {i, 1},
+                {str, <<"id">>} => {bin, <<"deposit">>},
+                {str, <<"handler">>} => {str, <<"ff_deposit">>},
+                {str, <<"source">>} => LegacyAccount,
+                {str, <<"destination">>} => LegacyAccount,
+                {str, <<"body">>} => {arr, [{str, <<"tup">>}, {i, 123}, {bin, <<"RUB">>}]},
+                {str, <<"params">>} => {arr, [{str, <<"map">>}, {obj, #{
+                    {str, <<"destination">>} => {bin, <<"wallet_id">>},
+                    {str, <<"source">>} => {bin, <<"source_id">>}
+                }}]}
+            }}
+        ]}
+    ]},
+    LegacyEvent = {arr, [
+        {str, <<"tup">>},
+        {str, <<"ev">>},
+        {arr, [
+            {str, <<"tup">>},
+            {arr, [
+                {str, <<"tup">>},
+                {arr, [{str, <<"tup">>}, {i, 2020}, {i, 5}, {i, 25}]},
+                {arr, [{str, <<"tup">>}, {i, 19}, {i, 19}, {i, 10}]}
+            ]},
+            {i, 293305}
+        ]},
+        LegacyChange
+    ]},
+    MetadataCtx = #{<<"com.rbkmoney.wapi">> => #{<<"metadata">> => #{}}},
+    {DecodedLegacy, _Context} = unmarshal({event, undefined}, LegacyEvent, #{ctx => MetadataCtx}),
+    ModernizedBinary = marshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, DecodedLegacy),
+    Decoded = unmarshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, ModernizedBinary),
+    ?assertEqual(Event, Decoded).
+
+-spec created_v2_3_decoding_test() -> _.
+created_v2_3_decoding_test() ->
+    Deposit = #{
+        version => 3,
+        id => <<"deposit">>,
+        body => {123, <<"RUB">>},
+        created_at => 1590426777985,
+        domain_revision => 123,
+        party_revision => 321,
+        external_id => <<"external_id">>,
+        params => #{
+            wallet_id => <<"wallet_id">>,
+            source_id => <<"source_id">>
+        },
+        metadata => #{}
+    },
+    Change = {created, Deposit},
+    Event = {ev, {{{2020, 5, 25}, {19, 19, 10}}, 293305}, Change},
+
+    LegacyChange = {arr, [
+        {str, <<"tup">>},
+        {str, <<"created">>},
+        {arr, [
+            {str, <<"map">>},
+            {obj, #{
+                {str, <<"version">>} => {i, 2},
+                {str, <<"id">>} => {bin, <<"deposit">>},
+                {str, <<"transfer_type">>} => {str, <<"deposit">>},
+                {str, <<"body">>} => {arr, [{str, <<"tup">>}, {i, 123}, {bin, <<"RUB">>}]},
+                {str, <<"params">>} => {arr, [{str, <<"map">>}, {obj, #{
+                    {str, <<"wallet_id">>} => {bin, <<"wallet_id">>},
+                    {str, <<"source_id">>} => {bin, <<"source_id">>}
+                }}]},
+                {str, <<"domain_revision">>} => {i, 123},
+                {str, <<"party_revision">>} => {i, 321},
+                {str, <<"created_at">>} => {i, 1590426777985},
+                {str, <<"external_id">>} => {bin, <<"external_id">>}
+            }}
+        ]}
+    ]},
+    LegacyEvent = {arr, [
+        {str, <<"tup">>},
+        {str, <<"ev">>},
+        {arr, [
+            {str, <<"tup">>},
+            {arr, [
+                {str, <<"tup">>},
+                {arr, [{str, <<"tup">>}, {i, 2020}, {i, 5}, {i, 25}]},
+                {arr, [{str, <<"tup">>}, {i, 19}, {i, 19}, {i, 10}]}
+            ]},
+            {i, 293305}
+        ]},
+        LegacyChange
+    ]},
+    MetadataCtx = #{<<"com.rbkmoney.wapi">> => #{<<"metadata">> => #{}}},
+    {DecodedLegacy, _Context} = unmarshal({event, undefined}, LegacyEvent, #{ctx => MetadataCtx}),
     ModernizedBinary = marshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, DecodedLegacy),
     Decoded = unmarshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, ModernizedBinary),
     ?assertEqual(Event, Decoded).
@@ -404,6 +588,136 @@ adjustment_v0_3_decoding_test() ->
         LegacyChange
     ]},
     DecodedLegacy = unmarshal({event, undefined}, LegacyEvent),
+    ModernizedBinary = marshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, DecodedLegacy),
+    Decoded = unmarshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, ModernizedBinary),
+    ?assertEqual(Event, Decoded).
+
+-spec created_1_decoding_test() -> _.
+created_1_decoding_test() ->
+    Deposit = #{
+        version => 3,
+        id => <<"deposit">>,
+        status => pending,
+        body => {123, <<"RUB">>},
+        created_at => 1590426777985,
+        domain_revision => 123,
+        party_revision => 321,
+        external_id => <<"external_id">>,
+        params => #{
+            wallet_id => <<"wallet_id">>,
+            source_id => <<"source_id">>
+        },
+        metadata => #{}
+    },
+    Change = {created, Deposit},
+    Event = {ev, {{{2020, 5, 25}, {19, 19, 10}}, 293305}, Change},
+
+    LegacyEvent = {bin, base64:decode(<<"CwABAAAAGzIwMjAtMDUtMjVUMTk6MTk6MTAuMjkzMzA1WgwAA",
+        "gwAAQwAAQsABQAAAAdkZXBvc2l0CwABAAAACXdhbGxldF9pZAsAAgAAAAlzb3VyY2VfaWQMAAMKAAEAAAA",
+        "AAAAAewwAAgsAAQAAAANSVUIAAAwABgwAAQAACwAEAAAAC2V4dGVybmFsX2lkCwAHAAAAGDIwMjAtMDUtM",
+        "jVUMTc6MTI6NTcuOTg1WgoACAAAAAAAAAB7CgAJAAAAAAAAAUENAAoLDAAAAAAAAAAA">>)},
+
+    DecodedLegacy = unmarshal({event, 1}, LegacyEvent),
+    ModernizedBinary = marshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, DecodedLegacy),
+    Decoded = unmarshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, ModernizedBinary),
+    ?assertEqual(Event, Decoded).
+
+-spec p_transfer_1_decoding_test() -> _.
+p_transfer_1_decoding_test() ->
+    PTransfer = #{
+        id => <<"external_id">>,
+        final_cash_flow => #{
+            postings => []
+        }
+    },
+    Change = {p_transfer, {created, PTransfer}},
+    Event = {ev, {{{2020, 5, 25}, {19, 19, 10}}, 293305}, Change},
+
+    LegacyEvent = {bin, base64:decode(<<"CwABAAAAGzIwMjAtMDUtMjVUMTk6MTk6MTAuMjkzMzA1WgwAAgwAA",
+        "wwAAQwAAQwAAQsAAgAAAAtleHRlcm5hbF9pZAwAAQ8AAQwAAAAAAAAAAAAAAA==">>)},
+
+    DecodedLegacy = unmarshal({event, 1}, LegacyEvent),
+    ModernizedBinary = marshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, DecodedLegacy),
+    Decoded = unmarshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, ModernizedBinary),
+    ?assertEqual(Event, Decoded).
+
+-spec limit_check_1_decoding_test() -> _.
+limit_check_1_decoding_test() ->
+    Change = {limit_check, {wallet_sender, ok}},
+    Event = {ev, {{{2020, 5, 25}, {19, 19, 10}}, 293305}, Change},
+
+    LegacyEvent = {bin, base64:decode(<<"CwABAAAAGzIwMjAtMDUtMjVUMTk6MTk6MTAuMjkzMzA1WgwAAgwABg",
+        "wAAQwAAQwAAQAAAAAAAA==">>)},
+
+    DecodedLegacy = unmarshal({event, 1}, LegacyEvent),
+    ModernizedBinary = marshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, DecodedLegacy),
+    Decoded = unmarshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, ModernizedBinary),
+    ?assertEqual(Event, Decoded).
+
+-spec revert_1_decoding_test() -> _.
+revert_1_decoding_test() ->
+    Revert = #{
+        id => <<"id">>,
+        payload => {created, #{
+            id => <<"deposit_revert">>,
+            status => pending,
+            body => {123, <<"RUB">>},
+            created_at => 1590426777985,
+            domain_revision => 123,
+            party_revision => 321,
+            external_id => <<"external_id">>,
+            wallet_id => <<"wallet_id">>,
+            source_id => <<"source_id">>
+        }}
+    },
+    Change = {revert, Revert},
+    Event = {ev, {{{2020, 5, 25}, {19, 19, 10}}, 293305}, Change},
+
+    LegacyEvent = {bin, base64:decode(<<"CwABAAAAGzIwMjAtMDUtMjVUMTk6MTk6MTAuMjkzMzA1WgwAAgwABAs",
+        "AAQAAAAJpZAwAAgwAAQwAAQsAAQAAAA5kZXBvc2l0X3JldmVydAsAAgAAAAl3YWxsZXRfaWQLAAMAAAAJc291cm",
+        "NlX2lkDAAEDAABAAAMAAUKAAEAAAAAAAAAewwAAgsAAQAAAANSVUIAAAsABgAAABgyMDIwLTA1LTI1VDE3OjEyO",
+        "jU3Ljk4NVoKAAcAAAAAAAAAewoACAAAAAAAAAFBCwAKAAAAC2V4dGVybmFsX2lkAAAAAAAA">>)},
+
+    DecodedLegacy = unmarshal({event, 1}, LegacyEvent),
+    ModernizedBinary = marshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, DecodedLegacy),
+    Decoded = unmarshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, ModernizedBinary),
+    ?assertEqual(Event, Decoded).
+
+-spec adjustment_1_decoding_test() -> _.
+adjustment_1_decoding_test() ->
+    CashFlowChange = #{
+        old_cash_flow_inverted => #{postings => []},
+        new_cash_flow => #{postings => []}
+    },
+
+    Plan = #{
+        new_cash_flow => CashFlowChange,
+        new_status => #{
+            new_status => succeeded
+        }
+    },
+    Adjustment = #{
+        id => <<"adjustment">>,
+        payload => {created, #{
+            id => <<"adjustment">>,
+            status => pending,
+            changes_plan => Plan,
+            created_at => 1590426777985,
+            domain_revision => 123,
+            party_revision => 321,
+            operation_timestamp => 1590426777986,
+            external_id => <<"external_id">>
+        }}
+    },
+    Change = {adjustment, Adjustment},
+    Event = {ev, {{{2020, 5, 25}, {19, 19, 10}}, 293305}, Change},
+
+    LegacyEvent = {bin, base64:decode(<<"CwABAAAAGzIwMjAtMDUtMjVUMTk6MTk6MTAuMjkzMzA1WgwAAgwABQsAA",
+        "QAAAAphZGp1c3RtZW50DAACDAABDAABCwABAAAACmFkanVzdG1lbnQMAAIMAAEAAAwAAwwAAQwAAQ8AAQwAAAAAAA",
+        "wAAg8AAQwAAAAAAAAMAAIMAAEMAAIAAAAACwAEAAAAGDIwMjAtMDUtMjVUMTc6MTI6NTcuOTg1WgoABQAAAAAAAAB",
+        "7CgAGAAAAAAAAAUELAAcAAAALZXh0ZXJuYWxfaWQLAAgAAAAYMjAyMC0wNS0yNVQxNzoxMjo1Ny45ODZaAAAAAAAA">>)},
+
+    DecodedLegacy = unmarshal({event, 1}, LegacyEvent),
     ModernizedBinary = marshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, DecodedLegacy),
     Decoded = unmarshal({event, ?CURRENT_EVENT_FORMAT_VERSION}, ModernizedBinary),
     ?assertEqual(Event, Decoded).

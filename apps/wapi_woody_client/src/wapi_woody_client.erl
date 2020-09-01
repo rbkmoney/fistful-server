728df19215660a39e028e3e951924afdc28a582d
Артем <WWW_cool@inbox.ru>
2020-09-01 16:53:08 +0300

    Revert "FF-156: Base withdrawal api (#284)" (#290)
    This reverts commit 87072db6dc557d4565cc207b5cfbb20885181438.

--- apps/wapi_woody_client/src/wapi_woody_client.erl
+++ apps/wapi_woody_client/src/wapi_woody_client.erl
@@ -88,8 +88,6 @@ get_service_modname(fistful_wallet) ->
     {ff_proto_wallet_thrift, 'Management'};
 get_service_modname(fistful_destination) ->
     {ff_proto_destination_thrift, 'Management'};
-get_service_modname(fistful_withdrawal) ->
-    {ff_proto_withdrawal_thrift, 'Management'};
 get_service_modname(webhook_manager) ->
     {ff_proto_webhooker_thrift, 'WebhookManager'}.
 

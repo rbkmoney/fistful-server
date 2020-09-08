b6e2a213a5dd70344cbf93b42aaa8c5fd8a85479
Roman Pushkov <pushkovroman@me.com>
2020-09-08 19:22:59 +0300

    add p2p transfer service

--- apps/wapi_woody_client/src/wapi_woody_client.erl
+++ apps/wapi_woody_client/src/wapi_woody_client.erl
@@ -92,6 +92,8 @@ get_service_modname(fistful_withdrawal) ->
     {ff_proto_withdrawal_thrift, 'Management'};
 get_service_modname(webhook_manager) ->
     {ff_proto_webhooker_thrift, 'WebhookManager'};
+get_service_modname(p2p_transfer) ->
+    {ff_proto_p2p_transfer_thrift, 'Management'};
 get_service_modname(w2w_transfer) ->
     {ff_proto_w2w_transfer_thrift, 'Management'}.
 

135d5551780f97978d205fec17dc3f5a9283519c
Roman Pushkov <pushkovroman@me.com>
2020-09-08 19:19:17 +0300

    update wapi access backend

--- apps/wapi/src/wapi_access_backend.erl
+++ apps/wapi/src/wapi_access_backend.erl
@@ -4,17 +4,25 @@
 -include_lib("fistful_proto/include/ff_proto_wallet_thrift.hrl").
 -include_lib("fistful_proto/include/ff_proto_destination_thrift.hrl").
 -include_lib("fistful_proto/include/ff_proto_w2w_transfer_thrift.hrl").
+-include_lib("fistful_proto/include/ff_proto_p2p_transfer_thrift.hrl").
 -include_lib("fistful_proto/include/ff_proto_withdrawal_thrift.hrl").
 
 -export([check_resource/3]).
 -export([check_resource_by_id/3]).
 
 -type id() :: binary().
--type resource_type() :: identity | wallet | destination | w2w_transfer | withdrawal.
+-type resource_type() :: identity
+                       | wallet
+                       | destination
+                       | withdrawal
+                       | w2w_transfer
+                       | p2p_transfer
+                       .
 
 -type handler_context() :: wapi_handler:context().
 -type data() ::
     ff_proto_identity_thrift:'IdentityState'() |
+    ff_proto_p2p_transfer_thrift:'P2PTransferState'() |
     ff_proto_wallet_thrift:'WalletState'().
 
 -define(CTX_NS, <<"com.rbkmoney.wapi">>).
@@ -76,6 +84,14 @@ get_context_by_id(w2w_transfer, W2WTransferID, WoodyCtx) ->
         {exception, #fistful_W2WNotFound{}} ->
             {error, notfound}
     end;
+get_context_by_id(p2p_transfer, P2PTransferID, WoodyCtx) ->
+    Request = {p2p_transfer, 'GetContext', [P2PTransferID]},
+    case wapi_handler_utils:service_call(Request, WoodyCtx) of
+        {ok, Context} ->
+            Context;
+        {exception, #fistful_P2PNotFound{}} ->
+            {error, notfound}
+    end;
 get_context_by_id(withdrawal, WithdrawalID, WoodyCtx) ->
     Request = {fistful_withdrawal, 'GetContext', [WithdrawalID]},
     case wapi_handler_utils:service_call(Request, WoodyCtx) of
@@ -92,6 +108,8 @@ get_context_from_state(wallet, #wlt_WalletState{context = Context}) ->
 get_context_from_state(destination, #dst_DestinationState{context = Context}) ->
     Context;
 get_context_from_state(w2w_transfer, #w2w_transfer_W2WTransferState{context = Context}) ->
+    Context;
+get_context_from_state(p2p_transfer, #p2p_transfer_P2PTransferState{context = Context}) ->
     Context;
 get_context_from_state(withdrawal, #wthd_WithdrawalState{context = Context}) ->
     Context.

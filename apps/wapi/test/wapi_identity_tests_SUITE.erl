74da4fa18a968bd89f97af023f6e124f5d517606
George Belyakov <8051393+georgemadskillz@users.noreply.github.com>
2020-10-12 23:12:14 +0300

    + wapi identity common test (#310)
    * + wapi identity common test
    
    * fix lint
    
    * fix lint
    
    * add test helper functions
    
    * fix context issue (return ct_helper_cfg call to call_api call)
    
    * fix function name
    
    Co-authored-by: y.beliakov <y.beliakov@rbkmoney.com>

--- apps/wapi/test/wapi_identity_tests_SUITE.erl
+++ apps/wapi/test/wapi_identity_tests_SUITE.erl
@@ -1,6 +1,7 @@
 -module(wapi_identity_tests_SUITE).
 
 -include_lib("common_test/include/ct.hrl").
+-include_lib("stdlib/include/assert.hrl").
 
 -include_lib("damsel/include/dmsl_webhooker_thrift.hrl").
 
@@ -23,13 +24,26 @@
 
 -export([
     create_identity/1,
+    create_identity_provider_notfound/1,
+    create_identity_class_notfound/1,
+    create_identity_party_inaccessible/1,
     create_identity_thrift_name/1,
     get_identity/1,
+    get_identity_notfound/1,
     create_identity_challenge/1,
+    create_identity_challenge_identity_notfound/1,
+    create_identity_challenge_challenge_pending/1,
+    create_identity_challenge_class_notfound/1,
+    create_identity_challenge_level_incorrect/1,
+    create_identity_challenge_conflict/1,
+    create_identity_challenge_proof_notfound/1,
+    create_identity_challenge_proof_insufficient/1,
     get_identity_challenge/1,
     list_identity_challenges/1,
+    list_identity_challenges_identity_notfound/1,
     get_identity_challenge_event/1,
-    poll_identity_challenge_events/1
+    poll_identity_challenge_events/1,
+    poll_identity_challenge_events_identity_notfound/1
 ]).
 
 % common-api is used since it is the domain used in production RN
@@ -63,13 +77,26 @@ groups() ->
         {base, [],
             [
                 create_identity,
+                create_identity_provider_notfound,
+                create_identity_class_notfound,
+                create_identity_party_inaccessible,
                 create_identity_thrift_name,
                 get_identity,
+                get_identity_notfound,
                 create_identity_challenge,
+                create_identity_challenge_identity_notfound,
+                create_identity_challenge_challenge_pending,
+                create_identity_challenge_class_notfound,
+                create_identity_challenge_level_incorrect,
+                create_identity_challenge_conflict,
+                create_identity_challenge_proof_notfound,
+                create_identity_challenge_proof_insufficient,
                 get_identity_challenge,
                 list_identity_challenges,
+                list_identity_challenges_identity_notfound,
                 get_identity_challenge_event,
-                poll_identity_challenge_events
+                poll_identity_challenge_events,
+                poll_identity_challenge_events_identity_notfound
             ]
         }
     ].
@@ -141,19 +168,39 @@ create_identity(C) ->
     wapi_ct_helper:mock_services([
         {fistful_identity, fun('Create', _) -> {ok, ?IDENTITY(PartyID)} end}
     ], C),
-    {ok, _} = call_api(
-        fun swag_client_wallet_identities_api:create_identity/3,
-        #{
-            body => #{
-                <<"name">> => ?STRING,
-                <<"class">> => ?STRING,
-                <<"provider">> => ?STRING,
-                <<"metadata">> => #{
-                    <<"somedata">> => ?STRING
-                }
-            }
-        },
-        ct_helper:cfg(context, C)
+    {ok, _} = create_identity_call_api(C).
+
+-spec create_identity_provider_notfound(config()) ->
+    _.
+create_identity_provider_notfound(C) ->
+    wapi_ct_helper:mock_services([
+        {fistful_identity, fun('Create', _) -> throw(#fistful_ProviderNotFound{}) end}
+    ], C),
+    ?assertEqual(
+        {error, {422, #{<<"message">> => <<"No such provider">>}}},
+        create_identity_call_api(C)
+    ).
+
+-spec create_identity_class_notfound(config()) ->
+    _.
+create_identity_class_notfound(C) ->
+    wapi_ct_helper:mock_services([
+        {fistful_identity, fun('Create', _) -> throw(#fistful_IdentityClassNotFound{}) end}
+    ], C),
+    ?assertEqual(
+        {error, {422, #{<<"message">> => <<"No such identity class">>}}},
+        create_identity_call_api(C)
+    ).
+
+-spec create_identity_party_inaccessible(config()) ->
+    _.
+create_identity_party_inaccessible(C) ->
+    wapi_ct_helper:mock_services([
+        {fistful_identity, fun('Create', _) -> throw(#fistful_PartyInaccessible{}) end}
+    ], C),
+    ?assertEqual(
+        {error, {422, #{<<"message">> => <<"Identity inaccessible">>}}},
+        create_identity_call_api(C)
     ).
 
 -spec create_identity_thrift_name(config()) ->
@@ -165,22 +212,7 @@ create_identity_thrift_name(C) ->
             {ok, ?IDENTITY(PartyID, ?DEFAULT_CONTEXT_NO_NAME(PartyID))}
         end}
     ], C),
-    {ok, #{
-        <<"name">> := ?STRING
-    }} = call_api(
-        fun swag_client_wallet_identities_api:create_identity/3,
-        #{
-            body => #{
-                <<"name">> => ?STRING,
-                <<"class">> => ?STRING,
-                <<"provider">> => ?STRING,
-                <<"metadata">> => #{
-                    <<"somedata">> => ?STRING
-                }
-            }
-        },
-        ct_helper:cfg(context, C)
-    ).
+    {ok, #{<<"name">> := ?STRING}} = create_identity_call_api(C).
 
 -spec get_identity(config()) ->
     _.
@@ -189,52 +221,89 @@ get_identity(C) ->
     wapi_ct_helper:mock_services([
         {fistful_identity, fun('Get', _) -> {ok, ?IDENTITY(PartyID)} end}
     ], C),
-    {ok, _} = call_api(
-        fun swag_client_wallet_identities_api:get_identity/3,
-        #{
-            binding => #{
-                <<"identityID">> => ?STRING
-            }
-        },
-        ct_helper:cfg(context, C)
+    {ok, _} = get_identity_call_api(C).
+
+-spec get_identity_notfound(config()) ->
+    _.
+get_identity_notfound(C) ->
+    wapi_ct_helper:mock_services([
+        {fistful_identity, fun('Get', _) -> throw(#fistful_IdentityNotFound{}) end}
+    ], C),
+    ?assertEqual(
+        {error, {404, #{}}},
+        get_identity_call_api(C)
     ).
 
 -spec create_identity_challenge(config()) ->
     _.
 create_identity_challenge(C) ->
-    PartyID = ?config(party, C),
-    wapi_ct_helper:mock_services([
-        {fistful_identity, fun
-            ('GetContext', _) -> {ok, ?DEFAULT_CONTEXT(PartyID)};
-            ('StartChallenge', _) -> {ok, ?IDENTITY_CHALLENGE(?IDENTITY_CHALLENGE_STATUS_COMPLETED)}
-        end},
-        {identdoc_storage, fun('Get', _) -> {ok, ?IDENT_DOC} end}
-    ], C),
-    {ok, _} = call_api(
-        fun swag_client_wallet_identities_api:start_identity_challenge/3,
-        #{
-            binding => #{
-                <<"identityID">> => ?STRING
-            },
-            body => #{
-                <<"type">> => <<"sword-initiation">>,
-                <<"proofs">> => [
-                    #{
-                        <<"token">> => wapi_utils:map_to_base64url(#{
-                            <<"type">> => <<"RUSRetireeInsuranceCertificate">>,
-                            <<"token">> => ?STRING
-                        })
-                    },
-                    #{
-                        <<"token">> => wapi_utils:map_to_base64url(#{
-                            <<"type">> => <<"RUSDomesticPassport">>,
-                            <<"token">> => ?STRING
-                        })
-                    }
-                ]
-            }
-        },
-        ct_helper:cfg(context, C)
+    create_identity_challenge_start_mocks(
+        C,
+        fun() -> {ok, ?IDENTITY_CHALLENGE(?IDENTITY_CHALLENGE_STATUS_COMPLETED)} end
+    ),
+    {ok, _} = create_identity_challenge_call_api(C).
+
+-spec create_identity_challenge_identity_notfound(config()) ->
+    _.
+create_identity_challenge_identity_notfound(C) ->
+    create_identity_challenge_start_mocks(C, fun() -> throw(#fistful_IdentityNotFound{}) end),
+    ?assertEqual(
+        {error, {404, #{}}},
+        create_identity_challenge_call_api(C)
+    ).
+
+-spec create_identity_challenge_challenge_pending(config()) ->
+    _.
+create_identity_challenge_challenge_pending(C) ->
+    create_identity_challenge_start_mocks(C, fun() -> throw(#fistful_ChallengePending{}) end),
+    ?assertEqual(
+        {error, {409, #{}}},
+        create_identity_challenge_call_api(C)
+    ).
+
+-spec create_identity_challenge_class_notfound(config()) ->
+    _.
+create_identity_challenge_class_notfound(C) ->
+    create_identity_challenge_start_mocks(C, fun() -> throw(#fistful_ChallengeClassNotFound{}) end),
+    ?assertEqual(
+        {error, {422, #{<<"message">> => <<"No such challenge type">>}}},
+        create_identity_challenge_call_api(C)
+    ).
+
+-spec create_identity_challenge_level_incorrect(config()) ->
+    _.
+create_identity_challenge_level_incorrect(C) ->
+    create_identity_challenge_start_mocks(C, fun() -> throw(#fistful_ChallengeLevelIncorrect{}) end),
+    ?assertEqual(
+        {error, {422, #{<<"message">> => <<"Illegal identification type for current identity level">>}}},
+        create_identity_challenge_call_api(C)
+    ).
+
+-spec create_identity_challenge_conflict(config()) ->
+    _.
+create_identity_challenge_conflict(C) ->
+    create_identity_challenge_start_mocks(C, fun() -> throw(#fistful_ChallengeConflict{}) end),
+    ?assertEqual(
+        {error, {409, #{}}},
+        create_identity_challenge_call_api(C)
+    ).
+
+-spec create_identity_challenge_proof_notfound(config()) ->
+    _.
+create_identity_challenge_proof_notfound(C) ->
+    create_identity_challenge_start_mocks(C, fun() -> throw(#fistful_ProofNotFound{}) end),
+    ?assertEqual(
+        {error, {422, #{<<"message">> => <<"Proof not found">>}}},
+        create_identity_challenge_call_api(C)
+    ).
+
+-spec create_identity_challenge_proof_insufficient(config()) ->
+    _.
+create_identity_challenge_proof_insufficient(C) ->
+    create_identity_challenge_start_mocks(C, fun() -> throw(#fistful_ProofInsufficient{}) end),
+    ?assertEqual(
+        {error, {422, #{<<"message">> => <<"Insufficient proof">>}}},
+        create_identity_challenge_call_api(C)
     ).
 
 -spec get_identity_challenge(config()) ->
@@ -283,6 +352,33 @@ list_identity_challenges(C) ->
         ct_helper:cfg(context, C)
     ).
 
+-spec list_identity_challenges_identity_notfound(config()) ->
+    _.
+list_identity_challenges_identity_notfound(C) ->
+    PartyID = ?config(party, C),
+    wapi_ct_helper:mock_services([
+        {fistful_identity, fun
+            ('GetContext', _) -> {ok, ?DEFAULT_CONTEXT(PartyID)};
+            ('GetChallenges', _) -> throw(#fistful_IdentityNotFound{})
+        end},
+        {identdoc_storage, fun('Get', _) -> {ok, ?IDENT_DOC} end}
+    ], C),
+    ?assertEqual(
+        {error, {404, #{}}},
+        call_api(
+            fun swag_client_wallet_identities_api:list_identity_challenges/3,
+            #{
+                binding => #{
+                    <<"identityID">> => ?STRING
+                },
+                qs_val => #{
+                    <<"status">> => <<"Completed">>
+                }
+            },
+            ct_helper:cfg(context, C)
+        )
+    ).
+
 -spec get_identity_challenge_event(config()) ->
     _.
 get_identity_challenge_event(C) ->
@@ -315,7 +411,103 @@ poll_identity_challenge_events(C) ->
             ('GetEvents', _) -> {ok, [?IDENTITY_CHALLENGE_EVENT(?CHALLENGE_STATUS_CHANGE)]}
         end}
     ], C),
-    {ok, _} = call_api(
+    {ok, _} = poll_identity_challenge_events_call_api(C).
+
+-spec poll_identity_challenge_events_identity_notfound(config()) ->
+    _.
+poll_identity_challenge_events_identity_notfound(C) ->
+    PartyID = ?config(party, C),
+    wapi_ct_helper:mock_services([
+        {fistful_identity, fun
+            ('GetContext', _) -> {ok, ?DEFAULT_CONTEXT(PartyID)};
+            ('GetEvents', _) -> throw(#fistful_IdentityNotFound{})
+        end}
+    ], C),
+    ?assertEqual(
+        {error, {404, #{}}},
+        poll_identity_challenge_events_call_api(C)
+    ).
+
+%%
+create_party(_C) ->
+    ID = genlib:bsuuid(),
+    _ = ff_party:create(ID),
+    ID.
+
+-spec call_api(function(), map(), wapi_client_lib:context()) ->
+    {ok, term()} | {error, term()}.
+call_api(F, Params, Context) ->
+    {Url, PreparedParams, Opts} = wapi_client_lib:make_request(Context, Params),
+    Response = F(Url, PreparedParams, Opts),
+    wapi_client_lib:handle_response(Response).
+
+create_identity_challenge_start_mocks(C, StartChallengeResultFun) ->
+    PartyID = ?config(party, C),
+    wapi_ct_helper:mock_services([
+        {fistful_identity, fun
+            ('GetContext', _) -> {ok, ?DEFAULT_CONTEXT(PartyID)};
+            ('StartChallenge', _) -> StartChallengeResultFun()
+        end},
+        {identdoc_storage, fun('Get', _) -> {ok, ?IDENT_DOC} end}
+    ], C).
+
+create_identity_call_api(C) ->
+    call_api(
+        fun swag_client_wallet_identities_api:create_identity/3,
+        #{
+            body => #{
+                <<"name">> => ?STRING,
+                <<"class">> => ?STRING,
+                <<"provider">> => ?STRING,
+                <<"metadata">> => #{
+                    <<"somedata">> => ?STRING
+                }
+            }
+        },
+        ct_helper:cfg(context, C)
+    ).
+
+get_identity_call_api(C) ->
+    call_api(
+        fun swag_client_wallet_identities_api:get_identity/3,
+        #{
+            binding => #{
+                <<"identityID">> => ?STRING
+            }
+        },
+        ct_helper:cfg(context, C)
+    ).
+
+create_identity_challenge_call_api(C) ->
+    call_api(
+        fun swag_client_wallet_identities_api:start_identity_challenge/3,
+        #{
+            binding => #{
+                <<"identityID">> => ?STRING
+            },
+            body => #{
+                <<"type">> => <<"sword-initiation">>,
+                <<"proofs">> => [
+                    #{
+                        <<"token">> => wapi_utils:map_to_base64url(#{
+                            <<"type">> => <<"RUSRetireeInsuranceCertificate">>,
+                            <<"token">> => ?STRING
+                        })
+                    },
+                    #{
+                        <<"token">> => wapi_utils:map_to_base64url(#{
+                            <<"type">> => <<"RUSDomesticPassport">>,
+                            <<"token">> => ?STRING
+                        })
+                    }
+                ]
+            }
+        },
+        ct_helper:cfg(context, C)
+    ).
+
+poll_identity_challenge_events_call_api(C) ->
+    call_api(
         fun swag_client_wallet_identities_api:poll_identity_challenge_events/3,
         #{
             binding => #{
@@ -330,16 +522,3 @@ poll_identity_challenge_events(C) ->
         ct_helper:cfg(context, C)
     ).
 
-%%
-
--spec call_api(function(), map(), wapi_client_lib:context()) ->
-    {ok, term()} | {error, term()}.
-call_api(F, Params, Context) ->
-    {Url, PreparedParams, Opts} = wapi_client_lib:make_request(Context, Params),
-    Response = F(Url, PreparedParams, Opts),
-    wapi_client_lib:handle_response(Response).
-
-create_party(_C) ->
-    ID = genlib:bsuuid(),
-    _ = ff_party:create(ID),
-    ID.

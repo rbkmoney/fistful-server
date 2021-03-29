d3fd242fe10ed2be102830f2dbb49ac2a5720bf1
dinama <dinama@users.noreply.github.com>
2021-03-29 23:41:43 +0300

    ED-96: move wapi ignoring to application level (#381)

--- apps/wapi/src/wapi.erl
+++ apps/wapi/src/wapi.erl
@@ -13,7 +13,15 @@
 
 -spec start(normal, any()) -> {ok, pid()} | {error, any()}.
 start(_StartType, _StartArgs) ->
-    wapi_sup:start_link().
+    case application:get_all_env(wapi) of
+        [] ->
+            % TODO #ED-96 на период разделения отсутствие настройки wapi -
+            % считаем признаком запуска только fistful-server
+            _ = logger:warning("wapi is not configured - start will be ignored"),
+            {ok, undefined};
+        _ ->
+            wapi_sup:start_link()
+    end.
 
 -spec stop(any()) -> ok.
 stop(_State) ->

ea2ffce105947ea146513895fb421618d06800ef
dinama <dinama@users.noreply.github.com>
2021-03-30 00:13:05 +0300

    Revert "ED-96: move wapi ignoring to application level (#381)"
    This reverts commit d3fd242fe10ed2be102830f2dbb49ac2a5720bf1.

--- apps/wapi/src/wapi.erl
+++ apps/wapi/src/wapi.erl
@@ -13,15 +13,7 @@
 
 -spec start(normal, any()) -> {ok, pid()} | {error, any()}.
 start(_StartType, _StartArgs) ->
-    case application:get_all_env(wapi) of
-        [] ->
-            % TODO #ED-96 на период разделения отсутствие настройки wapi -
-            % считаем признаком запуска только fistful-server
-            _ = logger:warning("wapi is not configured - start will be ignored"),
-            {ok, undefined};
-        _ ->
-            wapi_sup:start_link()
-    end.
+    wapi_sup:start_link().
 
 -spec stop(any()) -> ok.
 stop(_State) ->

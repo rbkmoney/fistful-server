1fe8e3e169057c45bfb0f455d3c4427495fd6101
Sergey Yelin <elinsn@gmail.com>
2021-03-30 11:58:32 +0300

    Use the same revision for adapter

--- apps/ff_transfer/src/ff_withdrawal_session.erl
+++ apps/ff_transfer/src/ff_withdrawal_session.erl
@@ -374,13 +374,12 @@ get_adapter_with_opts(ProviderID, TerminalID) when is_integer(ProviderID) ->
     DomainRevision = ff_domain_config:head(),
     {ok, Provider} = ff_payouts_provider:get(ProviderID, DomainRevision),
     ProviderOpts = ff_payouts_provider:adapter_opts(Provider),
-    TerminalOpts = get_adapter_terminal_opts(TerminalID),
+    TerminalOpts = get_adapter_terminal_opts(TerminalID, DomainRevision),
     {ff_payouts_provider:adapter(Provider), maps:merge(ProviderOpts, TerminalOpts)}.
 
-get_adapter_terminal_opts(undefined) ->
+get_adapter_terminal_opts(undefined, _DomainRevision) ->
     #{};
-get_adapter_terminal_opts(TerminalID) ->
-    DomainRevision = ff_domain_config:head(),
+get_adapter_terminal_opts(TerminalID, DomainRevision) ->
     {ok, Terminal} = ff_payouts_terminal:get(TerminalID, DomainRevision),
     ff_payouts_terminal:adapter_opts(Terminal).
 

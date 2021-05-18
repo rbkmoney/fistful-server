b2700aac17efee5628cf8e4735fb2d0d08d799d5
George Belyakov <y.beliakov@rbkmoney.com>
2021-05-18 17:32:38 +0300

    handle ProvisionTermSetUndefined exception

--- apps/ff_transfer/src/ff_withdrawal_routing.erl
+++ apps/ff_transfer/src/ff_withdrawal_routing.erl
@@ -229,17 +229,12 @@ get_valid_terminals_with_priority([{TerminalID, Priority} | Rest], ProviderRef, 
     | {error, Error :: term()}.
 validate_terms(ProviderRef, TerminalRef, PartyVarset, DomainRevision) ->
     case ff_party:compute_provider_terminal_terms(ProviderRef, TerminalRef, PartyVarset, DomainRevision) of
-        {ok, ProviderTerminalTermset} ->
-            case ProviderTerminalTermset of
-                #domain_ProvisionTermSet{
-                    wallet = #domain_WalletProvisionTerms{
-                        withdrawals = WithdrawalProvisionTerms
-                    }
-                } ->
-                    do_validate_terms(WithdrawalProvisionTerms, PartyVarset);
-                _ ->
-                    {error, {misconfiguration, {missing, withdrawal_terms}}}
-            end;
+        {ok, #domain_ProvisionTermSet{
+            wallet = #domain_WalletProvisionTerms{
+                withdrawals = WithdrawalProvisionTerms
+            }
+        }} ->
+            do_validate_terms(WithdrawalProvisionTerms, PartyVarset);
         {error, Error} ->
             {error, Error}
     end.

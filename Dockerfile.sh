6d83037ff6064eac673a8b13871005614ca9df23
Sergey Yelin <s.elin@rbkmoney.com>
2022-01-25 19:02:08 +0300

    ED-333: Redefine options for stdio/stderr (#413)

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 cat <<EOF
 FROM ${BASE_IMAGE}
-MAINTAINER Andrey Mayorov <a.mayorov@rbkmoney.com>
-COPY ./_build/prod/rel/${SERVICE_NAME} /opt/${SERVICE_NAME}
-CMD /opt/${SERVICE_NAME}/bin/${SERVICE_NAME} foreground
-EXPOSE 8022
 
-# wapi
-EXPOSE 8080
+COPY ./_build/prod/rel/fistful-server /opt/fistful-server
+EXPOSE 8022
 
 # A bit of magic below to get a proper branch name
 # even when the HEAD is detached (Hey Jenkins!
@@ -26,5 +22,7 @@ LABEL com.rbkmoney.${SERVICE_NAME}.parent=${BASE_IMAGE_NAME} \
         else \
           echo $(git name-rev --name-only HEAD); \
         fi)
-WORKDIR /opt
+
+WORKDIR /opt/fistful-server
+CMD /opt/fistful-server/bin/fistful-server foreground
 EOF
